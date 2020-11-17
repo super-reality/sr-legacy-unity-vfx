020df0a6be7825f88e6c606efc13b362a1f1853c
Neil Manansala <learlessfeader@gmail.com>
2020-11-16 17:18:53 -0800

    Implement basic effect controls

---
+++ Assets/SuperReality/Scripts/EffectManager.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+namespace SuperReality.Scripts
+{
+    public class EffectManager : MonoBehaviour
+    {
+        #region Fields
+
+        [SerializeField]
+        private UnityEvent onStart;
+
+        [SerializeField]
+        private UnityEvent onStop;
+
+        private bool _isPlaying;
+
+        #endregion
+
+        #region Methods
+
+        public void StartEffect()
+        {
+            if (_isPlaying) return;
+            _isPlaying = true;
+            onStart.Invoke();
+        }
+
+        public void StopEffect()
+        {
+            if (!_isPlaying) return;
+            _isPlaying = false;
+            onStop.Invoke();
+        }
+
+        #endregion
+    }
+}
