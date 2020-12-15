a2f0f2b7260d814dacab81288b447f362fbf1d61
cvives652 <31545090+cvives652@users.noreply.github.com>
2020-12-15 18:13:00 -0400

    Fantasy monster pack
    - Added the fantasy monster pack asset pack
    - Created the first overlay export

---
+++ Assets/FantasyMonsters/Scripts/Monster.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using Assets.FantasyMonsters.Scripts.Tweens;
+using UnityEngine;
+
+namespace Assets.FantasyMonsters.Scripts
+{
+    /// <summary>
+    /// The main script to control monsters.
+    /// </summary>
+    public class Monster : MonoBehaviour
+    {
+        public SpriteRenderer Head;
+        public List<Sprite> HeadSprites;
+        public Animator Animator;
+        public bool Variations;
+        public event Action<string> OnEvent = eventName => { };
+        
+        /// <summary>
+        /// Called on Awake.
+        /// </summary>
+        public void Awake()
+        {
+            if (Variations)
+            {
+                var variations = GetComponents<MonsterVariation>();
+                var random = UnityEngine.Random.Range(0, variations.Length + 1);
+
+                if (random > 0)
+                {
+                    variations[random - 1].Apply();
+                }
+            }
+
+            GetComponent<LayerManager>().SetSortingGroupOrder((int) -transform.localPosition.y);
+        }
+
+        /// <summary>
+        /// Set animation parameter State to control transitions. Play different state animations (except Attack).
+        /// </summary>
+        public void SetState(MonsterState state)
+        {
+            Animator.SetInteger("State", (int) state);
+        }
+
+        /// <summary>
+        /// Play Attack animation.
+        /// </summary>
+        public void Attack()
+        {
+            Animator.SetTrigger("Attack");
+        }
+
+        /// <summary>
+        /// Play scale spring animation.
+        /// </summary>
+        public virtual void Spring()
+        {
+            ScaleSpring.Begin(this, 1f, 1.1f, 40, 2);
+        }
+
+        // Play Die animation.
+        public void Die()
+        {
+            SetState(MonsterState.Death);
+        }
+
+        /// <summary>
+        /// Called from animation. Can be used by the game to handle animation events.
+        /// </summary>
+        public void Event(string eventName)
+        {
+            OnEvent(eventName);
+        }
+
+        /// <summary>
+        /// Called from animation.
+        /// </summary>
+        public void SetHead(int index)
+        {
+            if (index < HeadSprites.Count)
+            {
+                Head.sprite = HeadSprites[index];
+            }
+        }
+    }
+}
